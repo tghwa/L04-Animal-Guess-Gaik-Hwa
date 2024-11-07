4798bf08ede03411037cd45606d9586aa85bd466
Gaik Hwa <23053627@myrp.edu.sg>
2024-11-07 13:34:33 +0800

    Work Done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Text, View, ScrollView, Button, Alert} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, ScrollView, Button, Alert, StyleSheet } from 'react-native';
 import Animal from './animal';
 import RNPickerSelect from 'react-native-picker-select';
 
@@ -7,7 +7,7 @@ const Game = () => {
     let num = 0;
     const [selectedCrocodile, setCrocodile] = useState('');
     const [selectedDeer, setDeer] = useState('');
-    const [selectedElephant, setElaphant] = useState('');
+    const [selectedElephant, setElephant] = useState('');
     const [selectedGiraffe, setGiraffe] = useState('');
     const [selectedZebra, setZebra] = useState('');
 
@@ -31,82 +31,108 @@ const Game = () => {
         num = num + 1;
     }
 
-
-
     return (
-        <ScrollView>
-            <View>
-                <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'grey' }}>Guess the Animal - Tan Gaik Hwa</Text>
-                <Text></Text>
-                <Animal picture = {require('./img/zebra.jpg')} />
+        <ScrollView style={styles.container}>
+            <View style={styles.content}>
+                <Text style={styles.title}>Guess the Animal - Tan Gaik Hwa</Text>
+                <Text style={styles.subtitle}>Select the correct animal for each picture:</Text>
+
+                <Animal picture={require('./img/zebra.jpg')} />
                 <RNPickerSelect
                     onValueChange={(value) => setZebra(value)}
-                    items = {[
-                        {label: 'Crocodile', value: 'Crocodile'},
-                        {label: 'Deer', value: 'Deer'},
-                        {label: 'Elephant', value: 'Elephant'},
-                        {label: 'Giraffe', value: 'Giraffe'},
-                        {label: 'Zebra', value: 'Zebra'},
+                    items={[
+                        { label: 'Crocodile', value: 'Crocodile' },
+                        { label: 'Deer', value: 'Deer' },
+                        { label: 'Elephant', value: 'Elephant' },
+                        { label: 'Giraffe', value: 'Giraffe' },
+                        { label: 'Zebra', value: 'Zebra' },
                     ]}
-                    />
 
 
-                <Animal picture = {require('./img/giraffe.jpg')} />
+
+                />
+                <Text></Text>
+                <Animal picture={require('./img/giraffe.jpg')} />
                 <RNPickerSelect
                     onValueChange={(value) => setGiraffe(value)}
-                    items = {[
-                        {label: 'Crocodile', value: 'Crocodile'},
-                        {label: 'Deer', value: 'Deer'},
-                        {label: 'Elephant', value: 'Elephant'},
-                        {label: 'Giraffe', value: 'Giraffe'},
-                        {label: 'Zebra', value: 'Zebra'},
+                    items={[
+                        { label: 'Crocodile', value: 'Crocodile' },
+                        { label: 'Deer', value: 'Deer' },
+                        { label: 'Elephant', value: 'Elephant' },
+                        { label: 'Giraffe', value: 'Giraffe' },
+                        { label: 'Zebra', value: 'Zebra' },
                     ]}
+
                 />
-
-
-                <Animal picture = {require('./img/elephant.jpg')} />
+                <Text></Text>
+                <Animal picture={require('./img/elephant.jpg')} />
                 <RNPickerSelect
-                    onValueChange={(value) => setElaphant(value)}
-                    items = {[
-                        {label: 'Crocodile', value: 'Crocodile'},
-                        {label: 'Deer', value: 'Deer'},
-                        {label: 'Elephant', value: 'Elephant'},
-                        {label: 'Giraffe', value: 'Giraffe'},
-                        {label: 'Zebra', value: 'Zebra'},
+                    onValueChange={(value) => setElephant(value)}
+                    items={[
+                        { label: 'Crocodile', value: 'Crocodile' },
+                        { label: 'Deer', value: 'Deer' },
+                        { label: 'Elephant', value: 'Elephant' },
+                        { label: 'Giraffe', value: 'Giraffe' },
+                        { label: 'Zebra', value: 'Zebra' },
                     ]}
+
                 />
-
-
-                <Animal picture = {require('./img/deer.jpg')} />
+                <Text></Text>
+                <Animal picture={require('./img/deer.jpg')} />
                 <RNPickerSelect
                     onValueChange={(value) => setDeer(value)}
-                    items = {[
-                        {label: 'Crocodile', value: 'Crocodile'},
-                        {label: 'Deer', value: 'Deer'},
-                        {label: 'Elephant', value: 'Elephant'},
-                        {label: 'Giraffe', value: 'Giraffe'},
-                        {label: 'Zebra', value: 'Zebra'},
+                    items={[
+                        { label: 'Crocodile', value: 'Crocodile' },
+                        { label: 'Deer', value: 'Deer' },
+                        { label: 'Elephant', value: 'Elephant' },
+                        { label: 'Giraffe', value: 'Giraffe' },
+                        { label: 'Zebra', value: 'Zebra' },
                     ]}
+
                 />
-
-
-                <Animal picture = {require('./img/crocodile.jpg')} />
+                <Text></Text>
+                <Animal picture={require('./img/crocodile.jpg')} />
                 <RNPickerSelect
                     onValueChange={(value) => setCrocodile(value)}
-                    items = {[
-                        {label: 'Crocodile', value: 'Crocodile'},
-                        {label: 'Deer', value: 'Deer'},
-                        {label: 'Elephant', value: 'Elephant'},
-                        {label: 'Giraffe', value: 'Giraffe'},
-                        {label: 'Zebra', value: 'Zebra'},
+                    items={[
+                        { label: 'Crocodile', value: 'Crocodile' },
+                        { label: 'Deer', value: 'Deer' },
+                        { label: 'Elephant', value: 'Elephant' },
+                        { label: 'Giraffe', value: 'Giraffe' },
+                        { label: 'Zebra', value: 'Zebra' },
                     ]}
                 />
 
-                <Button title = "Submit" onPress={ () => Alert.alert("You've got " + num + ' out of 5 correct.') } />
-
+                <Button title="Submit" onPress={() => Alert.alert(`You've got ${num} out of 5 correct.`)} />
             </View>
         </ScrollView>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'grey',
+        padding: 10,
+    },
+    content: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'black',
+        marginBottom: 20,
+    },
+    subtitle: {
+        fontSize: 18,
+        color: 'black',
+        marginBottom: 10,
+    },
+
+});
+
 
 export default Game;
--- animal.js
+++ animal.js
@@ -1,18 +1,34 @@
-import { View, Text, Image } from 'react-native';
-
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 const Animal = ({ picture }) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Image
                 source={picture}
-                style={{ width: 400, height: 300 }}
+                style={styles.image}
             />
-            <Text style={{ fontSize: 20, color: 'grey' }}>What animal is this?</Text>
-
-            <Text></Text>
+            <Text style={styles.question}>What animal is this?</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    image: {
+        width: 350,
+        height: 250,
+        borderRadius: 10,
+        marginBottom: 10,
+    },
+    question: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black',
+        textAlign: 'center',
+    },
+});
 
 export default Animal;
